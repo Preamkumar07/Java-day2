b62b5739680a7368efa64bd481bebe28598c22b0
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-02-07 13:58:28 +0530

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,14 @@
+public class Arithemetic {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println(a+b);
+        System.out.println(b-a);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(b%a);
+        System.out.println("The sum:"+sum);
+        
+    }
+}
