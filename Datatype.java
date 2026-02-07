f186f4bc4ec8571b81f64bea045997515dd85b01
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-02-07 10:43:16 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+public class Datatype {
+    public static void main(String[] args) {
+        byte b=120;
+        short s=150;
+        int i=1002000;
+        long l=12300040l;
+        float f=5.23f;
+        double d=100.0d;
+        char c='A';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
