f85ac02cb295ab354f0167862e759cb00fb46a1a
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-02-07 10:55:24 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,14 @@
+public class Info {
+    public static void main(String[] args) {
+        String str="Pream Kumar";
+        System.out.println("Student Name:"+str);
+        int a=19;
+        System.out.println("Age:"+a);
+        int m=970;
+        System.out.println("Marks:"+m);
+        char g='A';
+        System.out.println("Grade:"+g);
+        String str1="Pass";
+        System.out.println("Student Status:"+str1);
+    }
+}
