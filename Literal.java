dcb0bc902111ce7653f07a92f972b2d38e2232b7
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-02-07 10:43:00 +0530

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,12 @@
+public class Literal {
+    public static void main(String[] args) {
+        int a=100;
+        int b=071;
+        int c=0x2A;
+        int d=0b100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
