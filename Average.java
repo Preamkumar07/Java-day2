0890df3c6db830fffc26a4eb332166bc914a8402
Kanipakapatnam PreamKumar <preammanasa123@gmail.com>
2026-02-07 11:07:14 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,14 @@
+public class Average {
+    public static void main(String[] args) {
+        int a=87;
+        int b=91;
+        int c=89;
+        int sum=a+b+c;
+        System.out.println("English:"+a);
+        System.out.println("Maths:"+b);
+        System.out.println("Physics:"+c);
+        System.out.println("The total sum:"+sum);
+        double avg=sum/3;
+        System.out.println("The Average:"+avg);
+    }
+}
